02048127defd1839a2fbb11ab1a5de6b03d254ce
Emilio <marqetm@gmail.com>
2022-05-30 18:14:54 -0500

    commit tree 645aaca2ded9ed0522965ea6a3ba452dca1402bc

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(param => {
+    return param.split(" ").map(word => {
+      return word[0].toUpperCase() + word.slice(1)
+    }).join(" ")
+  })
 }
+
+console.log(titleCased())
